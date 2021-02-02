f3960f92ecffaa19f215bb8620b935ffa59b9669
juancarlos90ntm <78409873+juancarlos90ntm@users.noreply.github.com>
2021-02-02 14:26:40 +0545

    Add files via upload

---
+++ script.py
@@ -0,0 +1,60 @@
+class script(object):
+
+    START_TEXT = """<b>Hi ,
+    
+    Hey, Nice to meet you. It's me ZEE5 video Downloader.
+    
+    Please send me any Zee5 link, I will give you that file in the form of File/Video.
+    
+        If found any bugs, report @NetworkingMindNTM !
+        Click <i>/help</i> for more details....</b>"""
+    Click <i>/about</i> conatact details </b>"""
+    
+ HELP_USER = """<b>Hi, Follow these steps </b>
+1. Send Custom Thumbnail (It will be saved permenantly!)
+2. Send your zee5 url and select desired option.
+NOTE: Download may take some time! So please wait for it to complete!"""
+
+  
+
+ABOUT_TEXT = """⭕️<b>My Name : ANS</b>
+
+⭕️<b>Creater :</b> @JuanCarlos90
+
+⭕️<b>Language :</b> <code>Python3</code>
+
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+
+⭕️<b>Source Code :</b> 👉 <a href='https://github.com/JuanCarlos90ntm/Zee5-Downloader'>Click Here</a>"""
+
+  
+    FORMAT_SELECTION = """<b>Choose appropriate option</b> <a href='{}'>⬇️</a>
+
+🎞  - Stream format
+📁  - File format
+
+<i>NOTE : Taking high resolutions may result in files above 2GB and Hence cannot Upload to Telegram. So better select a medium resolution.</i> 
+"""    
+    
+    UPGRADE_TEXT = "PING at ANS"
+    
+    DOWNLOAD_START = "Trying to download to my server. This may take a while"
+    
+    UPLOAD_START = "Uploading Now ⬆️"
+    
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
+
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "**Thank you for Using Me!! ❤️**"
+    
+    SAVED_CUSTOM_THUMB_NAIL = "<b>✅Custom thumbnail Saved.\nThis thumbnail will be Permanent for all future uploads\n\nDo /delthumb to clear your thumbnail!</b>"
+    
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom Thumbnail cleared succesfully."
+    
+    SHOW_THUMB = "@NetworkingMindNTM\n\nUse /delthumb to clear this thumbnail."
+    
+    NO_THUMB = "No saved thumbnails Found!!"
+    
+    CUSTOM_CAPTION_UL_FILE = "<b>{newname}\n\n©️ @NTM6Gbot</b>"
+    
+    TIMEOUT = "<b><i>Sorry for the delay. It'll help reduce the flood wait</i> 😇\n\ & Wait for {} sec and try again.</b>"
+    
